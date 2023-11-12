80e6e533317baabce0edb315d9353708d7ec040e
DocCreeps <dorian1998dino@hotmail.com>
2023-11-12 14:36:43 +0100

    Category model and migration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,11 +1,23 @@
 <?php
 
+// app/Models/Category.php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class, 'article_categories');
+    }
+
+    // Ajoutez d'autres relations et méthodes au besoin
 }
