d2cf1198daa8b06c1e95ba0ca08cefd3f6e41067
DocCreeps <dorian1998dino@hotmail.com>
2023-11-12 14:59:12 +0100

    User follow Migration and Model

--- database/migrations/2023_11_12_131057_create_user_follows_table.php
+++ database/migrations/2023_11_12_131057_create_user_follows_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_follows', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('follow_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
