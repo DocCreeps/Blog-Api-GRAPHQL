1d1d528200b50a527d6ac195993abf5eb94896e6
DocCreeps <dorian1998dino@hotmail.com>
2023-11-12 14:57:39 +0100

    LikeComment Migration and Model

--- app/Models/LikeComment.php
+++ app/Models/LikeComment.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LikeComment extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'comment_id', 'is_like'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function lists(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class);
+    }
+
 }
--- database/migrations/2023_11_12_131037_create_like_comments_table.php
+++ database/migrations/2023_11_12_131037_create_like_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('like_comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('comment_id')->constrained('comments')->cascadeOnDelete();
+            $table->boolean('is_like')->default(true);
             $table->timestamps();
         });
     }
